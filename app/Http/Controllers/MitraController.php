6910b9c1017cbae74a74db5de006b9418a6ea41a
kuamillll <blastergmly@gmail.com>
2024-09-09 12:10:45 +0700

    t

--- app/Http/Controllers/MitraController.php
+++ app/Http/Controllers/MitraController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mitra;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MitraController extends Controller
@@ -9,17 +11,34 @@ class MitraController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request):View
     {
         //
+        $search = $request->search;
+        $tanggal = $request->tanggal;
+
+        $mitras = Mitra::query();
+
+        if (!empty($search)) {
+            $mitras->where('nama_program', 'like', "%$search%")->orWhere('harga', 'like', "%$search%")->orWhere('judul', 'like', "%$search%")->orWhere('keterangan', 'like', "%$search%");
+        }
+
+        if (!empty($judul)) {
+            $mitras->where('judul', $judul);
+        }
+
+        $programs = $mitras->paginate(10);
+
+        return view('administrator.mitra.index', compact(['mitras']));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create():View
     {
         //
+        return view('administrator.mitra.create');
     }
 
     /**
